f0df69692ed416a3155fa3ef561e88a6cdd5ca3e
Thomas M. Vogel <vogel@startplus.de>
2013-07-20 16:02:59 +0200

    Apply Apache 2.0 License Info

--- ProfitBricks_API_Inventory_Demo.ps1
+++ ProfitBricks_API_Inventory_Demo.ps1
@@ -1,21 +1,20 @@
 ﻿########################################################################
-# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-########################################################################
-# Consume ProfitBricks SOAP-Api using Powershell
-# This is for demonstration purpose only
+# Copyright 2013 Thomas Vogel
 #
-# This example does not include any error or exeption handling
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#########################################################################
 # Code Repository: https://github.com/th-m-vogel/ProfitBricks-Webservice-PS-Samples
 #########################################################################
-# (c) ProfitBricks, 2013, Autor: Thomas Vogel
-########################################################################
 
 ## Set the URI to the WSLD
 $pb_wsdl = "https://api.profitbricks.com/1.2/wsdl"
@@ -35,6 +34,7 @@ $pb_creds = Get-Credential -Message "ProfitBricks Account"
 # $_user = "username@domain.top"
 # $pb_creds = New-Object System.Management.Automation.PsCredential($_user,$_password)
 # end import password from file 
+##
 
 ## add the credentials for api access (common)
 $pb_api.Credentials = $pb_creds
